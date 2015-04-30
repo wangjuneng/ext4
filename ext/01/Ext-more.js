2bf49412ecb2477c287b5bbeb7653623dba349bf
apple <apple@promote.cache-dns.local>
2015-04-30 20:44:35 +0800

    isReady

--- ext/01/Ext-more.js
+++ ext/01/Ext-more.js
@@ -1,6 +1,27 @@
 // JavaScript Document
-
- 
- 
- 
- 
+Ext.apply(Ext,{
+	userAgent:navigator.userAgent.toLowerCase(),
+	cache:{},
+	isSeed:1000,
+	
+	windowId:'ext-window',
+	documentId:'ext-document',
+	
+	isReady:false,
+	
+	enableGarbageCollector:true,
+	
+	enableListenerCollection:true,
+	
+	rootHierarchyState:{},
+	
+	addCacheEntry:function(id,el,dom)
+	{
+		dom = dom || el.dom;
+		
+		if(!dom)
+		{
+			Ext.Error.raise('Cannot add an entry to element cache without the dom node');
+		}
+	}
+});
