75e186e45e20cd15d1803926045cd41cdd9eea40
wangjuneng <wangjuneng@126.com>
2015-04-30 22:47:07 +0800

    check browser type

--- ext/01/Ext-more.js
+++ ext/01/Ext-more.js
@@ -217,5 +217,106 @@ window.undefined = window.undefined;
 
 //check browser type
 (function(){
+	var check = function(regex){
+		return regex.test(Ext.userAgent);
+	}
 	
+	isStrict = document.compatMode == 'CSS1Compat',
+	version = function(is,regex)
+	{
+		var m ;
+		return (is && ( m = regex.test(Ext.userAgent))) ? parseFloat(m[1]):0;
+	},
+	docMode = document.documentMode,
+	isOpera = check(/opera/),
+	isOpera10_5 = isOpera && check(/version\/10\.5/),
+	isChrome = check(/\bchrome\b/),
+	isWebKit = check(/webkit/),
+	isSafari = !isChrome && check(/safari/),
+	isSafari2 = isSafari && check(/applewebkit\/4/),
+	isSafari3 = isSafari && check(/version\/3/),
+	isSafari4 = isSafari && check(/version\/4/),
+	isSafari5_0 = isSafari && check(/version\/5.0/),
+	isSafari_5 = isSafari && check(/version\/5/),
+	isIE = !isOpera && check(/msie/),
+	isIE7 = isIE && ((check(/msie 7/) && docMode !=8 && docMode !=9 && docMode !=10 ) || docMode ==7),
+	isIE8 = isIE && ((check(/msie 8/) && docMode !=7 && docMode !=9 && docMode !=10 ) || docMode ==8),
+	isIE9 = isIE && ((check(/msie 9/) && docMode !=8 && docMode !=7 && docMode !=10 ) || docMode ==9),
+	isIE10 = isIE && ((check(/msie 9/) && docMode !=8 && docMode !=7 && docMode !=9 ) || docMode ==10),
+	isIE6 = isIE && ( check(/msie 6/)  ),
+	isGecko = !isWebKit && check(/gecko/),
+	isGecko3 = isGecko && check(/rv:1\.9/),
+	isGecko4 = isGecko && check(/rv:2\.0/),
+	isGecko5 = isGecko && check(/rv:5\./),
+	isGecko10 = isGecko && check(/rv:10\./),
+	isFF3_0 = isGecko3 && check(/rv:1\.9\.0/),
+	isFF3_5 = isGecko3 && check(/rv:1\.9\.1/),
+    isFF3_6 = isGecko3 && check(/rv:1\.9\.2/),
+	isWindow = check(/window|win32/),
+	isMac = check(/macintosh|mac os x/),
+	isLinux = check(/linux/),
+	scrollbarSize = null,
+	chromeVersion = version(true,/\bchrome\/(\d+\.\d+)/),
+	firefoxVersion = version(true,/\bfirefox\/(\d+\.\d+)/),
+	ieVersion = version(isIE,/msie (\d+\.\d+)/),
+	operaVersion = version(isOpera, /version\/(\d+\.\d+)/),
+    safariVersion = version(isSafari, /version\/(\d+\.\d+)/),
+    webKitVersion = version(isWebKit, /webkit\/(\d+\.\d+)/),
+	isSecure = /^https/i.test(window.location.protocal),
+	nullLog=null;
+	
+	try{
+		document.execCommand('BackgroundImageCache',false,true);
+	}catch(e){}
+	
+	var primitiveRe = /string|number|boolean/;
+	
+	function dumpObject(object)
+	{
+		var member,type,value,name,numbers=[];
+		
+		for(name in object)
+		{
+			if(object.hasOwnProperty(name))
+			{
+				value = object[name];
+				
+				type = typeof value;
+				
+				if(type=='function')
+				{
+					continue;
+				}
+				if(type=='undefined')
+				{
+					member = type;
+				}
+				else if(value===null || primitiveRe.test(type) || Ext.isDate(value))
+				{
+					member = Ext.decode(value);
+				}
+				else if(Ext.isArray(value))
+				{
+					member = '[ ]';
+				}else if(Ext.isObject(value))
+				{
+					member = '{ }';
+				}
+				else
+				{
+					member = type;
+				}
+				
+				members.push(Ext.encode(name) + ": " + member);
+				
+			}
+			
+			
+		}
+		if(members.length)
+		{
+			return '\n Data:{\n '+members.join(',\n   ') + '\n}';
+		}
+	}
+	console.log(dumpObject({a:2,b:4,d:5,f:new Date()}));
 }());
