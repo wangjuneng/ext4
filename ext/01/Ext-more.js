95aae0f7664cba51e067a3ba66ea4fec51665ef1
wangjuneng <wangjuneng@126.com>
2015-05-01 21:34:40 +0800

    Ext-more end

--- ext/01/Ext-more.js
+++ ext/01/Ext-more.js
@@ -237,7 +237,7 @@ window.undefined = window.undefined;
 	isSafari3 = isSafari && check(/version\/3/),
 	isSafari4 = isSafari && check(/version\/4/),
 	isSafari5_0 = isSafari && check(/version\/5.0/),
-	isSafari_5 = isSafari && check(/version\/5/),
+	isSafari5 = isSafari && check(/version\/5/),
 	isIE = !isOpera && check(/msie/),
 	isIE7 = isIE && ((check(/msie 7/) && docMode !=8 && docMode !=9 && docMode !=10 ) || docMode ==7),
 	isIE8 = isIE && ((check(/msie 8/) && docMode !=7 && docMode !=9 && docMode !=10 ) || docMode ==8),
@@ -252,7 +252,7 @@ window.undefined = window.undefined;
 	isFF3_0 = isGecko3 && check(/rv:1\.9\.0/),
 	isFF3_5 = isGecko3 && check(/rv:1\.9\.1/),
     isFF3_6 = isGecko3 && check(/rv:1\.9\.2/),
-	isWindow = check(/window|win32/),
+	isWindows = check(/window|win32/),
 	isMac = check(/macintosh|mac os x/),
 	isLinux = check(/linux/),
 	scrollbarSize = null,
@@ -318,7 +318,7 @@ window.undefined = window.undefined;
 			return '\n Data:{\n '+members.join(',\n   ') + '\n}';
 		}
 	}
-	console.log(dumpObject({a:2,b:4,d:5,f:new Date()}));
+	//console.log(dumpObject({a:2,b:4,d:5,f:new Date()}));
 	
 	
 	function log(message)
@@ -331,7 +331,7 @@ window.undefined = window.undefined;
 			
 			if(Ext.type(message) !='string')
 			{
-				options = messages;
+				options = message;
 				message = options.msg || '';
 				level = options.level || level;
 				dump = options.dump;
@@ -376,7 +376,7 @@ window.undefined = window.undefined;
 						con.log(message);
 					}
 					
-					if(dummp)
+					if(dump)
 					{
 						con.dir(dump);
 					}
@@ -430,10 +430,10 @@ window.undefined = window.undefined;
 	
 	log.info = function()
 	{
-		logx('info',Array.prototype.slice.call(arugments));	
+		logx('info',Array.prototype.slice.call(arguments));	
 	}
 	
-	log.warn = funciton()
+	log.warn = function()
 	{
 		logx('warn',Array.prototype.slice.call(arguments));	
 	}
@@ -446,7 +446,7 @@ window.undefined = window.undefined;
 	
 	log.out = [];
 	
-	lag.max = 750;
+	log.max = 750;
 	
 	    log.show = function () {
         window.open('','extlog').document.write([
@@ -467,9 +467,9 @@ window.undefined = window.undefined;
     };
 	
 	nullLog = function(){};
-	nullLog.info = nullLog.warn = nummLog.error = Ext.emptyFn;
+	nullLog.info = nullLog.warn = nullLog.error = Ext.emptyFn;
 	//update ext version
-	Ext.setVersion('extjs','4.2.1.883');
+	//Ext.setVersion('extjs','4.2.1.883');
 	
 	Ext.apply(Ext,{
 		SSL_SECURE_URL : isSecure && isIE ? 'javascript:\'\'':'about:blank',
@@ -518,7 +518,531 @@ window.undefined = window.undefined;
 					return el;
 				}
 			}
-		}
-	});
+		},
+		
+		removeNode:isIE6 || isIE7 || isIE8 ?
+			(function(){
+				var d ;
+				return function(n){
+					if(n && n.tagName.toUpperCase() != 'BODY')
+					{
+						(Eet.enableNestedListerRemoval )?Ext.EventManager.purgeElement(n):Ext.EventManager.removeAll(n);
+						
+						var cache = Ext.cache,id = n.id;
+						
+						if(cache[id])
+						{
+							delete cache[id].dom;
+							delete cache[id];
+						}
+						
+						if(isIE8 && n.parentNode)
+						{
+							n.parentNode.removeChild(n);
+						}
+						d = d|| document.createElement('div');
+						d.appendChild(n);
+						d.innerHTML = '';
+					}
+				}	
+			})() :function(n){
+			
+				if(n && n.parentNode && n.tagName.toUpperCase() !='BODY')
+				{
+					(Ext.enableNestedListenerRemoval) ? Ext.EventManager.purgeElement(n):Ext.EventManager.removeAll(n);
+					
+					var cahce = Ext.cache,id = n.id;
+					
+					if(cache[id])
+					{
+						delete cache[id].dom;
+						delete cache[id];
+					}
+					
+					n.parentNode.removeChild(n);
+				}
+			},
+		isStrict:isStrict,
+		
+		isIEQuirks :isIE && (!isStrict && (isIE6 || isIE7 || isIE8 || isIE9)),
+		
+		isOpera:isOpera,
+		
+		isOPera10_5:isOpera10_5,
+		
+		/**
+         * True if the detected browser uses WebKit.
+         * @type Boolean
+         */
+        isWebKit : isWebKit,
+
+        /**
+         * True if the detected browser is Chrome.
+         * @type Boolean
+         */
+        isChrome : isChrome,
+
+        /**
+         * True if the detected browser is Safari.
+         * @type Boolean
+         */
+        isSafari : isSafari,
+
+        /**
+         * True if the detected browser is Safari 3.x.
+         * @type Boolean
+         */
+        isSafari3 : isSafari3,
+
+        /**
+         * True if the detected browser is Safari 4.x.
+         * @type Boolean
+         */
+        isSafari4 : isSafari4,
+
+        /**
+         * True if the detected browser is Safari 5.x.
+         * @type Boolean
+         */
+        isSafari5 : isSafari5,
+
+        /**
+         * True if the detected browser is Safari 5.0.x.
+         * @type Boolean
+         */
+        isSafari5_0 : isSafari5_0,
+
+
+        /**
+         * True if the detected browser is Safari 2.x.
+         * @type Boolean
+         */
+        isSafari2 : isSafari2,
+
+        /**
+         * True if the detected browser is Internet Explorer.
+         * @type Boolean
+         */
+        isIE : isIE,
+
+        /**
+         * True if the detected browser is Internet Explorer 6.x.
+         * @type Boolean
+         */
+        isIE6 : isIE6,
+
+        /**
+         * True if the detected browser is Internet Explorer 7.x.
+         * @type Boolean
+         */
+        isIE7 : isIE7,
+
+        /**
+         * True if the detected browser is Internet Explorer 7.x or lower.
+         * @type Boolean
+         */
+        isIE7m : isIE6 || isIE7,
+
+        /**
+         * True if the detected browser is Internet Explorer 7.x or higher.
+         * @type Boolean
+         */
+        isIE7p : isIE && !isIE6,
+
+        /**
+         * True if the detected browser is Internet Explorer 8.x.
+         * @type Boolean
+         */
+        isIE8 : isIE8,
+
+        /**
+         * True if the detected browser is Internet Explorer 8.x or lower.
+         * @type Boolean
+         */
+        isIE8m : isIE6 || isIE7 || isIE8,
+
+        /**
+         * True if the detected browser is Internet Explorer 8.x or higher.
+         * @type Boolean
+         */
+        isIE8p : isIE && !(isIE6 || isIE7),
+
+        /**
+         * True if the detected browser is Internet Explorer 9.x.
+         * @type Boolean
+         */
+        isIE9 : isIE9,
+
+        /**
+         * True if the detected browser is Internet Explorer 9.x or lower.
+         * @type Boolean
+         */
+        isIE9m : isIE6 || isIE7 || isIE8 || isIE9,
+
+        /**
+         * True if the detected browser is Internet Explorer 9.x or higher.
+         * @type Boolean
+         */
+        isIE9p : isIE && !(isIE6 || isIE7 || isIE8),
+        
+        /**  
+         * True if the detected browser is Internet Explorer 10.x.
+         * @type Boolean
+         */
+        isIE10 : isIE10, 
+ 
+        /**
+         * True if the detected browser is Internet Explorer 10.x or lower.
+         * @type Boolean
+         */
+        isIE10m : isIE6 || isIE7 || isIE8 || isIE9 || isIE10,
+ 
+        /**
+         * True if the detected browser is Internet Explorer 10.x or higher.
+         * @type Boolean
+         */
+        isIE10p : isIE && !(isIE6 || isIE7 || isIE8 || isIE9),
+
+        /**
+         * True if the detected browser uses the Gecko layout engine (e.g. Mozilla, Firefox).
+         * @type Boolean
+         */
+        isGecko : isGecko,
+
+        /**
+         * True if the detected browser uses a Gecko 1.9+ layout engine (e.g. Firefox 3.x).
+         * @type Boolean
+         */
+        isGecko3 : isGecko3,
+
+        /**
+         * True if the detected browser uses a Gecko 2.0+ layout engine (e.g. Firefox 4.x).
+         * @type Boolean
+         */
+        isGecko4 : isGecko4,
+
+        /**
+         * True if the detected browser uses a Gecko 5.0+ layout engine (e.g. Firefox 5.x).
+         * @type Boolean
+         */
+        isGecko5 : isGecko5,
+
+        /**
+         * True if the detected browser uses a Gecko 5.0+ layout engine (e.g. Firefox 5.x).
+         * @type Boolean
+         */
+        isGecko10 : isGecko10,
+
+        /**
+         * True if the detected browser uses FireFox 3.0
+         * @type Boolean
+         */
+        isFF3_0 : isFF3_0,
+
+        /**
+         * True if the detected browser uses FireFox 3.5
+         * @type Boolean
+         */
+        isFF3_5 : isFF3_5,
+
+        /**
+         * True if the detected browser uses FireFox 3.6
+         * @type Boolean
+         */
+        isFF3_6 : isFF3_6,
+
+        /**
+         * True if the detected browser uses FireFox 4
+         * @type Boolean
+         */
+        isFF4 : 4 <= firefoxVersion && firefoxVersion < 5,
+
+        /**
+         * True if the detected browser uses FireFox 5
+         * @type Boolean
+         */
+        isFF5 : 5 <= firefoxVersion && firefoxVersion < 6,
+
+        /**
+         * True if the detected browser uses FireFox 10
+         * @type Boolean
+         */
+        isFF10 : 10 <= firefoxVersion && firefoxVersion < 11,
+
+        /**
+         * True if the detected platform is Linux.
+         * @type Boolean
+         */
+        isLinux : isLinux,
+
+        /**
+         * True if the detected platform is Windows.
+         * @type Boolean
+         */
+        isWindows : isWindows,
+
+        /**
+         * True if the detected platform is Mac OS.
+         * @type Boolean
+         */
+        isMac : isMac,
+
+        /**
+         * The current version of Chrome (0 if the browser is not Chrome).
+         * @type Number
+         */
+        chromeVersion: chromeVersion,
+
+        /**
+         * The current version of Firefox (0 if the browser is not Firefox).
+         * @type Number
+         */
+        firefoxVersion: firefoxVersion,
+
+        /**
+         * The current version of IE (0 if the browser is not IE). This does not account
+         * for the documentMode of the current page, which is factored into {@link #isIE7},
+         * {@link #isIE8} and {@link #isIE9}. Thus this is not always true:
+         *
+         *     Ext.isIE8 == (Ext.ieVersion == 8)
+         *
+         * @type Number
+         */
+        ieVersion: ieVersion,
+
+        /**
+         * The current version of Opera (0 if the browser is not Opera).
+         * @type Number
+         */
+        operaVersion: operaVersion,
+
+        /**
+         * The current version of Safari (0 if the browser is not Safari).
+         * @type Number
+         */
+        safariVersion: safariVersion,
+
+        /**
+         * The current version of WebKit (0 if the browser does not use WebKit).
+         * @type Number
+         */
+        webKitVersion: webKitVersion,
+
+        /**
+         * True if the page is running over SSL
+         * @type Boolean
+         */
+        isSecure: isSecure,
+		
+		BLANK_IMAGE_URL:(isIE6 || isIE7) ? '/' +'www.sencha.com/s.gif' : 'data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==',
+		value:function(v,defaultValue,allowBlank)
+		{
+			return Ext.isEmpty(v,allowBlank) ?defaultValue:v;
+		},
+		// escape regular expression
+		escapeRe:function(s)
+		{
+			return s.replace(/([-.*+?\^${}()|\[\]\/\\])/g, "\\$1");
+		},
+		addBehaviors:function(o)
+		{
+			if(!Ext.isReady)
+			{
+				Ext.onReady(function(){
+					Ext.addBehaviors(o);
+				});
+			}
+			else
+			{
+				var cache = {}, parts,b,s;
+				for(b in o)
+				{
+					if((parts = b.split('@'))[1])
+					{
+						s = parts[0];
+						if(!cache[s])
+						{
+							cache[s] = Ext.select(s);
+						}
+						
+						cache[s].on(parts[1],o[b]);
+					}
+				}
+				
+				cache = null;
+			}
+		},
+		
+		getScrollbarSize:function(force)
+		{
+			if(!Ext.isReady)
+			{
+				return {};
+			}
+			
+			if(force || !scorllbarSize)
+			{
+				var db = document.body,div = document.createElement('div');
+				
+				div.style.width = div.style.height = '100px';
+				div.style.overflow = 'scroll';
+				div.style.position = 'absolute';
+				
+				db.appendChild(div);
+				
+				scrollbarSize = {
+					width :div.offsetWidth - div.clientWidth,
+					height:div.offsetHeight - div.clientHeight	
+				};
+				
+				db.removeChild(div);
+			}
+			
+			return scrollbarSize;
+		},
+		
+		getScrollBarWidth:function(force)
+		{
+			var size = Ext.getScrollbarSize(force);
+			return size.width +2;
+		},
+		copyTo:function(dest,source,names,usePrototypeKeys)
+		{
+			if(Ext.type(names)=='string')
+			{
+				names = names.split(/[,;\s]/);
+			}
+			
+			var n,nLen = names ? names.length :0,name;
+			for(n =0 ;n<nLen;n++)
+			{
+				if(userPrototypeKeys || score.hasOwnProperty(name))
+				{
+					dest[name]=source[name];
+				}
+			}
+			
+			return dest;
+		},
+		destoryMembers:function(o)
+		{
+			for(var i=1,a=arguemnts,len = a.length;i<len;i++)
+			{
+				Ext.destory(a[i]);
+				delete o[a[i]];
+			}
+		},
+		log:log || nullLog,
+		partition:function(arr,truth)
+		{
+			var ret = [[],[]],a,v,aLen = arr.length;
+			
+			for(a = 0;a<aLen;a++)
+			{
+				v =arr[a];
+				ret[(truth && truth(v,a,arr)) || (!truth && v) ? 0 :1].push(v);
+			}
+		},
+		invoke:function(arr,methodName)
+		{
+			var ret = [],args = Array.prototype.slice.call(arguments,2),
+				a,v,aLen = arr.length;
+			for(a = 0;a<aLen;a++)
+			{
+				v = arr[a];
+				if(v && typeof v[methodName]=='function')
+				{
+					ret.push(v[methodName.apply(v,args));
+				}
+				else
+				{
+					ret.push(undefined);
+				}
+			}
+			
+			return ret;
+			
+		},
+		zip:function()
+		{
+			var parts = Ext.partition(arguments,function(val){return typeof val!='function';}),
+			arrs = parts[0],
+			fn:parts[1][0],
+			len:Ext.max(Ext.pluck(arrs,'length')),
+			ret = [],i,j,aLen;
+			
+			for(i=0;i<len;i++)
+			{
+				ret[i] = [];
+				if(fn)
+				{
+					ret[i]=fn.apply(fn,Ext.pluck(arrs,i));
+				}
+				else
+				{
+					for(j=0,aLen = arrs.length;j<aLen;j++)
+					{
+						ret[i].push(arrs[j][i]);
+					}
+				}
+			}
+			
+			return ret;
+		},
+		toSentence:function(items,connector)
+		{
+			var length = items.length,head,tail;
+			if(length<1)
+			{
+				return items[0];
+			}
+			else
+			{
+				head = item.slice(0,length-1);
+				tail = item[length-1];
+				return Ext.util.Format.format("{0} {1} {2}",head.join(", "),eonnector || 'and' ,tail);
+			}
+		},
+		seteGlyhpFontFamily:function(fontFamily)
+		{
+			Ext._glyphFontFamily = ontFamily;
+		},
+		useShims:isIE6
+		
+	});//end Ext.apply(Ext,{});
 	
 }());
+Ext.application = function(config)
+{
+	var App,paths,ns;
+	
+	if(typeof config === 'string')
+	{
+		Ext.require(config,function(){
+			App = Ext.ClassManager.get(config);	
+		});
+	}
+	else
+	{
+		Ext.loader.setPath(config.name,config.appFolder |'app');
+		
+		if(paths = config.paths)
+		{
+			for(ns in paths)
+			{
+				if(paths.hasOwnPropety(ns))
+				{
+					Ext.Loader.setPath(ns,paths[ns]);
+				}
+			}
+		}
+		
+		config['paths processed'] = true;
+		
+		Ext.define(config.name + '.$application',Ext.apply({extend:'Ext.app.Application'},config),function(){App = this});
+	}
+	
+	Ext.onReady(function(){
+		Ext.app.Application.instance = new App();	
+	});
+}
